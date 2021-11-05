b9145e152f37c2e1c7852757ebfee28605203a2a
reno <83641492+Renohar@users.noreply.github.com>
2021-11-05 19:26:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const todoText = document.getElementById("todo-text");
+const todoul = document.getElementById("todo-ul");
+
+function addTask(){
+    if (todoText.value.trim() === ""){
+        alert("Enter some tasks");
+    }
+    else{
+        console.log(todoText.value.trim());
+        const liTag = document.createElement("li");
+        const spanText = document.createElement("span");
+        const textNode = document.createTextNode(todoText.value.trim());
+        spanText.appendChild(textNode);
+        spanText.addEventListener("click",(e)=>{
+            console.log(e.target);
+            e.target.classList.toggle('done');
+        })
+        const spanClose = document.createElement("span");
+        spanClose.classList.add("close");
+        spanClose.innerHTML = "Delete";
+        spanClose.addEventListener("click",(e)=>{
+            console.log(e.target.parentElement);
+            const li = e.target.parentElement;
+            li.parentElement.removeChild(li);
+        })
+        liTag.appendChild(spanText);
+        liTag.appendChild(spanClose);
+        todoul.appendChild(liTag);
+        todoText.value="";
+    }
+}
